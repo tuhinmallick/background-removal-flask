3d4e40464338cdbf54a7498a2071096d36f15b74
Tuhin Mallick <tuhin.mllk@gmail.com>
2023-08-25 23:29:36 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
-from serverless_wsgi import handle_request  # Make sure you have this package in your requirements
 from flask import Flask, jsonify
 from app.api.endpoints.background_removal import background_removal
 from app.api.endpoints.human_segmentation import human_segment
 from app.api.endpoints.segment_and_remove_background import segment_and_remove_background_bp as segment_and_remove_bg
 import logging
+
 logging.basicConfig(level=logging.DEBUG)
 
 def create_app():
@@ -16,7 +16,6 @@ def create_app():
     Returns:
         app: A Flask application instance.
     """
-
     app = Flask(__name__)
 
     # Register blueprints with versioning
@@ -27,22 +26,7 @@ def create_app():
 
     return app
 
-def lambda_handler(event, context):
-    """
-    Handles the lambda event and context and returns the result.
-
-    Args:
-        event (dict): The event data passed to the lambda function.
-        context (dict): The context object passed to the lambda function.
-
-    Returns:
-        The result of calling the handle_request function with the app, event, and context parameters.
-    """  
-    logger.debug("Starting lambda...")
-    return handle_request(app, event, context)
-
 app = create_app()
 
-
 if __name__ == '__main__':
-    app.run(port=5002)
+    app.run(host='0.0.0.0', port=8000)
