4004b40000f6e78c44a1156ac772b110682dad35
Tuhin Mallick <tuhin.mllk@gmail.com>
2023-08-25 21:34:13 +0200

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,8 +0,0 @@
-from .u2net_portrait import detect_single_face
-from .u2net_portrait import crop_face
-from .u2net_portrait import normPRED
-from .u2net_portrait import inference
-from .u2net_portrait import process_portrait_image
-
-
-__all__ = [ 'detect_single_face', 'crop_face', 'normPRED', 'inference', 'DatabaseManager', 'process_portrait_image' ]
